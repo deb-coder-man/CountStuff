9d55a0549622aaf9c7e3b7caf9059e2e69a22d01
Dave <dkha055@aucklanduni.ac.nz>
2025-11-03 15:33:19 +1300

    Fixing CORS configuration in backend

--- backend/app.py
+++ backend/app.py
@@ -8,7 +8,7 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://localhost:3000"],
+    allow_origins=["http://localhost:5173", "http://localhost:3000", "https://count-stuff.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
